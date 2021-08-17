b6b436f944bc264c5180a22246de792785eadf14
mdann1992 <mauricedann@web.de>
2021-08-17 16:48:34 +0200

    Add default springBoot-Project with Web-dependecies, SQL-dependencies and MySQL-dependecies

---
+++ casestudy/src/test/java/de/maurice/casestudy/CasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.maurice.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
